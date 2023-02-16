8e763b136853cc3f829f5f75650c9ba0c862ad27
wanghailu <wanghailu0717@163.com>
2023-02-16 16:07:21 +0800

    add MobileNet

---
+++ MobileNet/mobilenet.py
@@ -0,0 +1,287 @@
+# 引入 torch 包、神经网络包、函数包、优化器
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+import torch.optim as optim
+# 引入图像包、图像处理包、显示包、时间包
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.transforms import ToPILImage
+import time
+
+# 定义网络并显示
+class DepthSeperabelConv2d(nn.Module):
+
+    def __init__(self, input_channels, output_channels, kernel_size, **kwargs):
+        super().__init__()
+        self.depthwise = nn.Sequential(
+            nn.Conv2d(
+                input_channels,
+                input_channels,
+                kernel_size,
+                groups=input_channels,
+                **kwargs),
+            nn.BatchNorm2d(input_channels),
+            nn.ReLU(inplace=True)
+        )
+
+        self.pointwise = nn.Sequential(
+            nn.Conv2d(input_channels, output_channels, 1),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+
+        return x
+
+
+class BasicConv2d(nn.Module):
+
+    def __init__(self, input_channels, output_channels, kernel_size, **kwargs):
+
+        super().__init__()
+        self.conv = nn.Conv2d(
+            input_channels, output_channels, kernel_size, **kwargs)
+        self.bn = nn.BatchNorm2d(output_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+
+        return x
+
+
+class MobileNet(nn.Module):
+
+    """
+    Args:
+        width multipler: The role of the width multiplier α is to thin
+                         a network uniformly at each layer. For a given
+                         layer and width multiplier α, the number of
+                         input channels M becomes αM and the number of
+                         output channels N becomes αN.
+    """
+
+    def __init__(self, width_multiplier=1, class_num=100):
+       super().__init__()
+
+       alpha = width_multiplier
+       self.stem = nn.Sequential(
+           BasicConv2d(3, int(32 * alpha), 3, padding=1, bias=False),
+           DepthSeperabelConv2d(
+               int(32 * alpha),
+               int(64 * alpha),
+               3,
+               padding=1,
+               bias=False
+           )
+       )
+
+       #downsample
+       self.conv1 = nn.Sequential(
+           DepthSeperabelConv2d(
+               int(64 * alpha),
+               int(128 * alpha),
+               3,
+               stride=2,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(128 * alpha),
+               int(128 * alpha),
+               3,
+               padding=1,
+               bias=False
+           )
+       )
+
+       #downsample
+       self.conv2 = nn.Sequential(
+           DepthSeperabelConv2d(
+               int(128 * alpha),
+               int(256 * alpha),
+               3,
+               stride=2,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(256 * alpha),
+               int(256 * alpha),
+               3,
+               padding=1,
+               bias=False
+           )
+       )
+
+       #downsample
+       self.conv3 = nn.Sequential(
+           DepthSeperabelConv2d(
+               int(256 * alpha),
+               int(512 * alpha),
+               3,
+               stride=2,
+               padding=1,
+               bias=False
+           ),
+
+           DepthSeperabelConv2d(
+               int(512 * alpha),
+               int(512 * alpha),
+               3,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(512 * alpha),
+               int(512 * alpha),
+               3,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(512 * alpha),
+               int(512 * alpha),
+               3,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(512 * alpha),
+               int(512 * alpha),
+               3,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(512 * alpha),
+               int(512 * alpha),
+               3,
+               padding=1,
+               bias=False
+           )
+       )
+
+       #downsample
+       self.conv4 = nn.Sequential(
+           DepthSeperabelConv2d(
+               int(512 * alpha),
+               int(1024 * alpha),
+               3,
+               stride=2,
+               padding=1,
+               bias=False
+           ),
+           DepthSeperabelConv2d(
+               int(1024 * alpha),
+               int(1024 * alpha),
+               3,
+               padding=1,
+               bias=False
+           )
+       )
+
+       self.fc = nn.Linear(int(1024 * alpha), class_num)
+       self.avg = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        x = self.stem(x)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = self.avg(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+def mobilenet(alpha=1, class_num=10):
+    return MobileNet(alpha, class_num)
+
+net = mobilenet()
+print(net)
+
+# 定义数据预处理方式以及训练集与测试集并进行下载
+transform = transforms.Compose([
+        transforms.ToTensor(), # 转为Tensor
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), # 归一化
+                             ])
+trainset = torchvision.datasets.CIFAR10(
+                    root='../data', 
+                    train=True, 
+                    download=True,
+                    transform=transform)
+trainloader = torch.utils.data.DataLoader(
+                    trainset, 
+                    batch_size=4,
+                    shuffle=True, 
+                    num_workers=2)
+testset = torchvision.datasets.CIFAR10(
+                    '../data',
+                    train=False, 
+                    download=True, 
+                    transform=transform)
+testloader = torch.utils.data.DataLoader(
+                    testset,
+                    batch_size=4, 
+                    shuffle=False,
+                    num_workers=2)
+
+# 定义损失函数与优化器
+criterion = nn.CrossEntropyLoss() # 交叉熵损失函数
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# 定义硬件设备
+device = torch.device("mlu")
+net = net.to(device)
+
+# 网络训练
+start = time.time()
+for epoch in range(2):  
+    running_loss = 0.0
+    start_0 = time.time()
+    for i, data in enumerate(trainloader, 0):
+        # 输入数据
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        # 梯度清零
+        optimizer.zero_grad()
+        # 前向传播、计算损失、反向计算、参数更新
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # 打印日志
+        running_loss += loss.item()
+        if i % 2000 == 1999: # 每2000个batch打印一下训练状态
+            end_2000 = time.time()
+            print('[%d, %5d] loss: %.3f take %.5f s' \
+                  % (epoch+1, i+1, running_loss / 2000, (end_2000-start_0)))
+            running_loss = 0.0
+            start_0 = time.time()
+end = time.time()
+print('Finished Training: ' + str(end- start) + 's')
+
+# 网络推理
+correct = 0 # 预测正确的图片数
+total = 0 # 总共的图片数
+# 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+print('10000张测试集中的准确率为: %d %%' % (100 * correct / total))
---
+++ MobileNet/mobilenet_v2.py
@@ -0,0 +1,179 @@
+# 引入 torch 包、神经网络包、函数包、优化器
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+import torch.optim as optim
+# 引入图像包、图像处理包、显示包、时间包
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.transforms import ToPILImage
+import time
+
+# 定义网络并显示
+class LinearBottleNeck(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride, t=6, class_num=100):
+        super().__init__()
+
+        self.residual = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels * t, 1),
+            nn.BatchNorm2d(in_channels * t),
+            nn.ReLU6(inplace=True),
+
+            nn.Conv2d(in_channels * t, in_channels * t, 3, stride=stride, padding=1, groups=in_channels * t),
+            nn.BatchNorm2d(in_channels * t),
+            nn.ReLU6(inplace=True),
+
+            nn.Conv2d(in_channels * t, out_channels, 1),
+            nn.BatchNorm2d(out_channels)
+        )
+
+        self.stride = stride
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+    def forward(self, x):
+
+        residual = self.residual(x)
+
+        if self.stride == 1 and self.in_channels == self.out_channels:
+            residual += x
+
+        return residual
+
+class MobileNetV2(nn.Module):
+
+    def __init__(self, class_num=100):
+        super().__init__()
+
+        self.pre = nn.Sequential(
+            nn.Conv2d(3, 32, 1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU6(inplace=True)
+        )
+
+        self.stage1 = LinearBottleNeck(32, 16, 1, 1)
+        self.stage2 = self._make_stage(2, 16, 24, 2, 6)
+        self.stage3 = self._make_stage(3, 24, 32, 2, 6)
+        self.stage4 = self._make_stage(4, 32, 64, 2, 6)
+        self.stage5 = self._make_stage(3, 64, 96, 1, 6)
+        self.stage6 = self._make_stage(3, 96, 160, 1, 6)
+        self.stage7 = LinearBottleNeck(160, 320, 1, 6)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(320, 1280, 1),
+            nn.BatchNorm2d(1280),
+            nn.ReLU6(inplace=True)
+        )
+
+        self.conv2 = nn.Conv2d(1280, class_num, 1)
+
+    def forward(self, x):
+        x = self.pre(x)
+        x = self.stage1(x)
+        x = self.stage2(x)
+        x = self.stage3(x)
+        x = self.stage4(x)
+        x = self.stage5(x)
+        x = self.stage6(x)
+        x = self.stage7(x)
+        x = self.conv1(x)
+        x = func.adaptive_avg_pool2d(x, 1)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+
+        return x
+
+    def _make_stage(self, repeat, in_channels, out_channels, stride, t):
+
+        layers = []
+        layers.append(LinearBottleNeck(in_channels, out_channels, stride, t))
+
+        while repeat - 1:
+            layers.append(LinearBottleNeck(out_channels, out_channels, 1, t))
+            repeat -= 1
+
+        return nn.Sequential(*layers)
+
+def mobilenetv2():
+    return MobileNetV2(class_num = 10)
+
+net = mobilenetv2()
+print(net)
+
+# 定义数据预处理方式以及训练集与测试集并进行下载
+transform = transforms.Compose([
+        transforms.ToTensor(), # 转为Tensor
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), # 归一化
+                             ])
+trainset = torchvision.datasets.CIFAR10(
+                    root='../data', 
+                    train=True, 
+                    download=True,
+                    transform=transform)
+trainloader = torch.utils.data.DataLoader(
+                    trainset, 
+                    batch_size=4,
+                    shuffle=True, 
+                    num_workers=2)
+testset = torchvision.datasets.CIFAR10(
+                    '../data',
+                    train=False, 
+                    download=True, 
+                    transform=transform)
+testloader = torch.utils.data.DataLoader(
+                    testset,
+                    batch_size=4, 
+                    shuffle=False,
+                    num_workers=2)
+
+# 定义损失函数与优化器
+criterion = nn.CrossEntropyLoss() # 交叉熵损失函数
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# 定义硬件设备
+device = torch.device("mlu")
+net = net.to(device)
+
+# 网络训练
+start = time.time()
+for epoch in range(2):  
+    running_loss = 0.0
+    start_0 = time.time()
+    for i, data in enumerate(trainloader, 0):
+        # 输入数据
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        # 梯度清零
+        optimizer.zero_grad()
+        # 前向传播、计算损失、反向计算、参数更新
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # 打印日志
+        running_loss += loss.item()
+        if i % 2000 == 1999: # 每2000个batch打印一下训练状态
+            end_2000 = time.time()
+            print('[%d, %5d] loss: %.3f take %.5f s' \
+                  % (epoch+1, i+1, running_loss / 2000, (end_2000-start_0)))
+            running_loss = 0.0
+            start_0 = time.time()
+end = time.time()
+print('Finished Training: ' + str(end- start) + 's')
+
+# 网络推理
+correct = 0 # 预测正确的图片数
+total = 0 # 总共的图片数
+# 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+print('10000张测试集中的准确率为: %d %%' % (100 * correct / total))
