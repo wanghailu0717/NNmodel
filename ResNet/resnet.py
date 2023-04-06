e82fbd9cb2cdab6682dbf143b944d4d600d182bc
wanghailu <wanghailu0717@163.com>
2023-04-06 15:42:56 +0800

    fix code

--- ResNet/resnet.py
+++ ResNet/resnet.py
@@ -8,11 +8,30 @@ import torchvision
 import torchvision.transforms as transforms
 from torchvision.transforms import ToPILImage
 import time
-# 引入onnx 相关工具
+# 引入 onnx 相关工具
 import onnx
 from onnxsim import simplify
 from onnx import version_converter
 from onnx2torch import convert
+# 引入参数解析包
+import argparse
+# 引入工具包
+import sys
+from tqdm import tqdm
+
+# 定义参数解析
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_batch", default="4", type=int, help="Default value of train_batch is 4.")
+parser.add_argument("--train_epoch", default="2", type=int, help="Default value of train_epoch is 2. Set 0 to this argument if you want an untrained network.")
+parser.add_argument("--infer_batch", default="1", type=int, help="Default value of infer_batch is 1.")
+parser.add_argument("--which_net", type=str, required=True, choices=["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"])
+parser.add_argument("--which_device", type=str, default="cpu", choices=["mlu", "cuda", "cpu"])
+parser.add_argument("--pytorch_infer", type=str, default="False", choices=["False", "True"])
+parser.add_argument("--gofusion_infer", type=str, default="False", choices=["False", "True"])
+parser.add_argument("--onnx_file", type=str, default="")
+args = parser.parse_args()
+
+print(vars(args))
 
 # 定义网络并显示
 class BasicBlock(nn.Module):
@@ -159,8 +178,6 @@ def resnet152():
     """
     return ResNet(BottleNeck, [3, 8, 36, 3], 10)
 
-net = resnet18()
-
 # 定义数据预处理方式以及训练集与测试集并进行下载
 transform = transforms.Compose([
         transforms.ToTensor(), # 转为Tensor
@@ -173,7 +190,7 @@ trainset = torchvision.datasets.CIFAR10(
                     transform=transform)
 trainloader = torch.utils.data.DataLoader(
                     trainset, 
-                    batch_size=4,
+                    batch_size=args.train_batch,
                     shuffle=True, 
                     num_workers=2)
 testset = torchvision.datasets.CIFAR10(
@@ -183,118 +200,149 @@ testset = torchvision.datasets.CIFAR10(
                     transform=transform)
 testloader = torch.utils.data.DataLoader(
                     testset,
-                    batch_size=1, 
+                    batch_size=args.infer_batch, 
                     shuffle=False,
                     num_workers=2)
-
+########################################################################
+# 定义网络
+if args.which_net == "resnet18":
+    net = resnet18()
+elif args.which_net == "resnet34":
+    net = resnet34()
+elif args.which_net == "resnet50":
+    net = resnet50()
+elif args.which_net == "resnet101":
+    net = resnet101()
+elif args.which_net == "resnet152":
+    net = resnet152()
+else:
+    print("Non-existent network")
+    sys.exit()
 # 定义损失函数与优化器
 criterion = nn.CrossEntropyLoss() # 交叉熵损失函数
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 # 定义硬件设备
-print(torch.cuda.is_available())
-device = torch.device("mlu")
+device = torch.device(args.which_device)
+input_rand = torch.zeros((1,3,32,32))
+torch.onnx.export(net, input_rand, args.which_net + '_untrained' + '.onnx', input_names = ["image"], output_names = ["label"])
 net = net.to(device)
 
-# # 网络训练
-# start = time.time()
-# for epoch in range(6):  
-#     running_loss = 0.0
-#     start_0 = time.time()
-#     for i, data in enumerate(trainloader, 0):
-#         # 输入数据
-#         inputs, labels = data
-#         inputs = inputs.to(device)
-#         labels = labels.to(device)
-#         # 梯度清零
-#         optimizer.zero_grad()
-#         # 前向传播、计算损失、反向计算、参数更新
-#         outputs = net(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#         # 打印日志
-#         running_loss += loss.item()
-#         if i % 2000 == 1999: # 每2000个batch打印一下训练状态
-#             end_2000 = time.time()
-#             print('[%d, %5d] loss: %.3f take %.5f s' \
-#                   % (epoch+1, i+1, running_loss / 2000, (end_2000-start_0)))
-#             running_loss = 0.0
-#             start_0 = time.time()
-# end = time.time()
-# print('Finished Training: ' + str(end- start) + 's')
+# 网络训练
+if args.train_epoch != 0:
+    print("[INFO] Strat training " + args.which_net + " network on " + args.which_device)
+    start = time.time()
+    for epoch in range(args.train_epoch):  
+        print("[INFO] Training " + str(epoch) + " epoch...")
+        running_loss = 0.0
+        start_0 = time.time()
+        for i, data in tqdm(enumerate(trainloader, 0)):
+            # 输入数据
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # 梯度清零
+            optimizer.zero_grad()
+            # 前向传播、计算损失、反向计算、参数更新
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # 打印日志
+            running_loss += loss.item()
+            if i % 2000 == 1999: # 每2000个batch打印一下训练状态
+                end_2000 = time.time()
+                print('[%d, %5d] loss: %.3f take %.5f s' \
+                      % (epoch+1, i+1, running_loss / 2000, (end_2000-start_0)))
+                running_loss = 0.0
+                start_0 = time.time()
+    end = time.time()
+    print('Finished Training: ' + str(end- start) + 's')
+    input_rand = torch.zeros((1,3,32,32))
+    net = net.to("cpu")
+    torch.onnx.export(net, input_rand, args.which_net + '.onnx', input_names = ["image"], output_names = ["label"])
 
 # 网络推理
-correct = 0 # 预测正确的图片数
-total = 0 # 总共的图片数
-
+# correct = 0 # 预测正确的图片数
+# total = 0 # 总共的图片数
 # 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum()
-print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
-
-# 网络存储与再捞回
-input_rand = torch.zeros((1,3,32,32))
-net = net.to("cpu")
-torch.onnx.export(net, input_rand, 'resnet18.onnx', input_names = ["image"], output_names = ["label"])
-model = onnx.load('./resnet18.onnx')
-
-# 本项目对模型进行优化
-model, check = simplify(model)
-# from pyinfinitensor.onnx import OnnxStub, cuda_runtime
-# gofusion_model = OnnxStub(model, cuda_runtime())
-# model = gofusion_model
+# with torch.no_grad():
+#     for data in testloader:
+#         images, labels = data
+#         images = images.to(device)
+#         labels = labels.to(device)
+#         outputs = net(images)
+#         _, predicted = torch.max(outputs, 1)
+#         total += labels.size(0)
+#         correct += (predicted == labels).sum()
+# print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
 
 ###################################################################################
 # Pytorch 运行
 # 将模型转换为对应版本
-target_version = 13
-converted_model = version_converter.convert_version(model, target_version)
-torch_model = convert(converted_model)
-torch_model.to(device)
-# 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
-correct = 0 # 预测正确的图片数
-total = 0 # 总共的图片数
-total_time = 0.0
-with torch.no_grad():
-    for data in testloader:
+if args.pytorch_infer == "True":
+    if len(args.onnx_file) != 0:
+        model = onnx.load(args.onnx_file)
+    else:
+        model = onnx.load('./'+ args.which_net +'.onnx')
+    model, check = simplify(model)
+
+    target_version = 13
+    converted_model = version_converter.convert_version(model, target_version)
+    torch_model = convert(converted_model)
+    torch_model.to(device)
+    torch_model.eval()
+    print("[INFO] Pytorch strat infer " + args.which_net + " network on " + args.which_device)
+    correct = 0 # 预测正确的图片数
+    total = 0 # 总共的图片数
+    total_time = 0.0
+    with torch.no_grad():
+        for data in tqdm(testloader):
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            start_time = time.time()
+            outputs = torch_model(images)
+            end_time = time.time()
+            total_time += (end_time - start_time)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+    print('%d 张测试的准确率为: %f %%' % (total, 100 * correct / total))
+    print('BatchSize = %d, Pytorch 推理耗时 %f s' % (labels.size(0), total_time / (total / (labels.size(0)))))
+    del torch_model, model
+
+###################################################################################
+# Gofusion 运行
+if args.gofusion_infer == "True":
+    if len(args.onnx_file) != 0:
+        model = onnx.load(args.onnx_file)
+    else:
+        model = onnx.load('./'+ args.which_net +'.onnx')
+    model, check = simplify(model)
+
+    from pyinfinitensor.onnx import OnnxStub, backend
+    gofusion_model = OnnxStub(model, backend.bang_runtime())
+    model = gofusion_model
+    print("[INFO] Gofusion strat infer " + args.which_net + " network on " + args.which_device)
+    correct = 0 # 预测正确的图片数
+    total = 0 # 总共的图片数
+    total_time = 0.0
+    # 使用本项目的 Runtime 运行刚才加载并转换的模型, 验证是否一致
+    for data in tqdm(testloader):
         images, labels = data
-        images = images.to(device)
-        labels = labels.to(device)
+        next(model.inputs.items().__iter__())[1].copyin_float(images.reshape(-1).tolist())
         start_time = time.time()
-        outputs = torch_model(images)
+        model.init()
+        model.run()
         end_time = time.time()
+        outputs = next(model.outputs.items().__iter__())[1].copyout_float()
+        outputs = torch.tensor(outputs)
+        outputs = torch.reshape(outputs,(1,10))
         total_time += (end_time - start_time)
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum()
-print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
-print('BatchSize = %d, Pytorch 推理耗时 %f s' % (labels.size(0), total_time / (total / (labels.size(0)))))
-
-correct = 0 # 预测正确的图片数
-total = 0 # 总共的图片数
-total_time = 0.0
-# 使用本项目的 Runtime 运行刚才加载并转换的模型, 验证是否一致
-for data in testloader:
-    images, labels = data
-    next(model.inputs.items().__iter__())[1].copyin_float(images.reshape(-1).tolist())
-    start_time = time.time()
-    model.run()
-    end_time = time.time()
-    outputs = next(model.outputs.items().__iter__())[1].copyin_float()
-    outputs = torch.tensor(outputs)
-    outputs = torch.reshape(outputs,(1,10))
-    total_time += (end_time - start_time)
-    _, predicted = torch.max(outputs, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
-print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
-print('BatchSize = %d, GoFusion 推理耗时 %f s' % (labels.size(0), total_time / (total / (labels.size(0)))))
-
+    print('%d 张测试的准确率为: %f %%' % (total, 100 * correct / total))
+    print('BatchSize = %d, GoFusion 推理耗时 %f s' % (labels.size(0), total_time / (total / (labels.size(0)))))
+    del model, gofusion_model
