6036e73c5770dbda8a633543c6250278c50d04fa
wanghailu <wanghailu0717@163.com>
2023-04-07 13:31:33 +0800

    update

--- Inception/inceptionv4.py
+++ Inception/inceptionv4.py
@@ -13,6 +13,24 @@ import onnx
 from onnxsim import simplify
 from onnx import version_converter
 from onnx2torch import convert
+# 引入参数解析包
+import argparse
+# 引入工具包
+import sys
+from tqdm import tqdm
+
+# 定义参数解析
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_batch", default="4", type=int, help="Default value of train_batch is 4.")
+parser.add_argument("--train_epoch", default="2", type=int, help="Default value of train_epoch is 2. Set 0 to this argument if you want an untrained network.")
+parser.add_argument("--infer_batch", default="1", type=int, help="Default value of infer_batch is 1.")
+parser.add_argument("--which_device", type=str, default="cpu", choices=["mlu", "cuda", "cpu"])
+parser.add_argument("--pytorch_infer", type=str, default="False", choices=["False", "True"])
+parser.add_argument("--gofusion_infer", type=str, default="False", choices=["False", "True"])
+parser.add_argument("--onnx_file", type=str, default="")
+args = parser.parse_args()
+
+print(vars(args))
 
 # 定义网络并显示
 class BasicConv2d(nn.Module):
@@ -562,7 +580,7 @@ trainset = torchvision.datasets.CIFAR10(
                     transform=transform)
 trainloader = torch.utils.data.DataLoader(
                     trainset, 
-                    batch_size=4,
+                    batch_size=args.train_batch,
                     shuffle=True, 
                     num_workers=2)
 testset = torchvision.datasets.CIFAR10(
@@ -572,7 +590,7 @@ testset = torchvision.datasets.CIFAR10(
                     transform=transform)
 testloader = torch.utils.data.DataLoader(
                     testset,
-                    batch_size=4, 
+                    batch_size=args.infer_batch, 
                     shuffle=False,
                     num_workers=2)
 
@@ -580,77 +598,126 @@ testloader = torch.utils.data.DataLoader(
 criterion = nn.CrossEntropyLoss() # 交叉熵损失函数
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 # 定义硬件设备
-device = torch.device("cuda")
+device = torch.device(args.which_device)
+input_rand = torch.zeros((1,3,32,32))
+torch.onnx.export(net, input_rand, 'inceptionv4' + '_untrained' + '.onnx', input_names = ["image"], output_names = ["label"])
 net = net.to(device)
 
 # 网络训练
-start = time.time()
-for epoch in range(2):  
-    running_loss = 0.0
-    start_0 = time.time()
-    for i, data in enumerate(trainloader, 0):
-        # 输入数据
-        inputs, labels = data
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        # 梯度清零
-        optimizer.zero_grad()
-        # 前向传播、计算损失、反向计算、参数更新
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        # 打印日志
-        running_loss += loss.item()
-        if i % 2000 == 1999: # 每2000个batch打印一下训练状态
-            end_2000 = time.time()
-            print('[%d, %5d] loss: %.3f take %.5f s' \
-                  % (epoch+1, i+1, running_loss / 2000, (end_2000-start_0)))
-            running_loss = 0.0
-            start_0 = time.time()
-end = time.time()
-print('Finished Training: ' + str(end- start) + 's')
+if args.train_epoch != 0:
+    print("[INFO] Strat training " + "inceptionv4" + " network on " + args.which_device)
+    start = time.time()
+    for epoch in range(args.train_epoch):  
+        running_loss = 0.0
+        start_0 = time.time()
+        for i, data in tqdm(enumerate(trainloader, 0)):
+            # 输入数据
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            # 梯度清零
+            optimizer.zero_grad()
+            # 前向传播、计算损失、反向计算、参数更新
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # 打印日志
+            running_loss += loss.item()
+            if i % 2000 == 1999: # 每2000个batch打印一下训练状态
+                end_2000 = time.time()
+                print('[%d, %5d] loss: %.3f take %.5f s' \
+                      % (epoch+1, i+1, running_loss / 2000, (end_2000-start_0)))
+                running_loss = 0.0
+                start_0 = time.time()
+    end = time.time()
+    print('Finished Training: ' + str(end- start) + 's')
+    input_rand = torch.zeros((1,3,32,32))
+    net = net.to("cpu")
+    torch.onnx.export(net, input_rand, 'inceptionv4' + '.onnx', input_names = ["image"], output_names = ["label"])
+    
+# # 网络推理
+# correct = 0 # 预测正确的图片数
+# total = 0 # 总共的图片数
+# # 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
+# with torch.no_grad():
+#     for data in testloader:
+#         images, labels = data
+#         images = images.to(device)
+#         labels = labels.to(device)
+#         outputs = net(images)
+#         _, predicted = torch.max(outputs, 1)
+#         total += labels.size(0)
+#         correct += (predicted == labels).sum()
+# 
+# print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
 
-# 网络推理
-correct = 0 # 预测正确的图片数
-total = 0 # 总共的图片数
-# 由于测试的时候不需要求导，可以暂时关闭autograd，提高速度，节约内存
-with torch.no_grad():
-    for data in testloader:
+# Gofusion 运行
+if args.gofusion_infer == "True":
+    if len(args.onnx_file) != 0:
+        model = onnx.load(args.onnx_file)
+    else:
+        model = onnx.load('./'+ 'inceptionv4' +'.onnx')
+    model, check = simplify(model)
+
+    from pyinfinitensor.onnx import OnnxStub, backend
+    gofusion_model = OnnxStub(model, backend.bang_runtime())
+    model = gofusion_model
+    print("[INFO] Gofusion strat infer " + 'inceptionv4' + " network on " + args.which_device)
+    correct = 0 # 预测正确的图片数
+    total = 0 # 总共的图片数
+    total_time = 0.0
+    # 使用本项目的 Runtime 运行刚才加载并转换的模型, 验证是否一致
+    for data in tqdm(testloader):
         images, labels = data
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = net(images)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum()
-
-print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
-
-# 网络存储与再捞回
-input_rand = torch.zeros((1,3,32,32))
-net = net.to("cpu")
-torch.onnx.export(net, input_rand, 'inceptionv4.onnx', input_names = ["image"], output_names = ["label"])
-model = onnx.load('./inceptionv4.onnx')
-
-# 本项目对模型进行优化
-model, check = simplify(model)
-from pyinfinitensor.onnx import OnnxStub, cuda_runtime
-gofusion_model = OnnxStub(model, cuda_runtime())
-model = gofusion_model
-
-correct = 0 # 预测正确的图片数
-total = 0 # 总共的图片数
-# 使用本项目的 Runtime 运行刚才加载并转换的模型, 验证是否一致
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        model.put_float(next(model.inputs.keys().__iter__()), images.reshape(-1).tolist())
+        next(model.inputs.items().__iter__())[1].copyin_float(images.reshape(-1).tolist())
+        start_time = time.time()
+        model.init()
         model.run()
-        outputs = model.take_float()
+        end_time = time.time()
+        outputs = next(model.outputs.items().__iter__())[1].copyout_float()
         outputs = torch.tensor(outputs)
         outputs = torch.reshape(outputs,(1,10))
+        total_time += (end_time - start_time)
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum()
-print('10000张测试集中的准确率为: %f %%' % (100 * correct / total))
+    print('%d 张测试的准确率为: %f %%' % (total, 100 * correct / total))
+    print('BatchSize = %d, GoFusion 推理耗时 %f s' % (labels.size(0), total_time / (total / (labels.size(0)))))
+    del model, gofusion_model
+
+###################################################################################
+# Pytorch 运行
+# 将模型转换为对应版本
+if args.pytorch_infer == "True":
+    if len(args.onnx_file) != 0:
+        model = onnx.load(args.onnx_file)
+    else:
+        model = onnx.load('./'+ 'inceptionv4' +'.onnx')
+    model, check = simplify(model)
+
+    target_version = 13
+    converted_model = version_converter.convert_version(model, target_version)
+    torch_model = convert(converted_model)
+    torch_model.to(device)
+    torch_model.eval()
+    print("[INFO] Pytorch strat infer " + "inceptionv4" + " network on " + args.which_device)
+    correct = 0 # 预测正确的图片数
+    total = 0 # 总共的图片数
+    total_time = 0.0
+    with torch.no_grad():
+        for data in tqdm(testloader):
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            start_time = time.time()
+            outputs = torch_model(images)
+            end_time = time.time()
+            total_time += (end_time - start_time)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+    print('%d 张测试的准确率为: %f %%' % (total, 100 * correct / total))
+    print('BatchSize = %d, Pytorch 推理耗时 %f s' % (labels.size(0), total_time / (total / (labels.size(0)))))
+    del torch_model, model
+
